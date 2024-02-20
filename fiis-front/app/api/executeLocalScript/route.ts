026977bf3d935720548a0fb0cc49be5dcdcf667a
verdant31 <joao.pedro3110@hotmail.com>
2024-02-20 19:36:45 -0400

    removing unused api routes

--- fiis-front/app/api/executeLocalScript/route.ts
+++ fiis-front/app/api/executeLocalScript/route.ts
@@ -5,7 +5,7 @@ const execAsync = util.promisify(exec);
 
 export async function POST(req: NextRequest) {
   try {
-    const { stdout } = await execAsync(`ts-node C:/Users/Verdant/Desktop/fiis/fiis-script/src/index.ts`);
+    const { stdout } = await execAsync(`npx ts-node /home/verdant/Desktop/fiis/fiis-script/src/index.ts`);
     return NextResponse.json({ message: "Script executed", status: 200, stdout });
   } catch (err) {
     return NextResponse.json({ message: (err as Error)?.message, status: 500 });
