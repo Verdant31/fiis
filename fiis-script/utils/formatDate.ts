4338ce7630d2840eb7831cb612012adf4fec8ed2
verdant31 <joao.pedro3110@hotmail.com>
2024-08-06 16:07:11 -0300

    changing project structure

--- fiis-script/utils/formatDate.ts
+++
@@ -1,8 +0,0 @@
-const { parse, format } = require("date-fns");
-
-export const convertDate = (date: string | undefined) => {
-  const parsed = parse(date, "dd/MM/yyyy", new Date());
-  const formatted = format(parsed, "yyyy-MM-dd");
-
-  return formatted;
-};
