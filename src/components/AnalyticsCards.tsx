a8c652b84b8a37f5cf9d28c8cdc17c50749361ad
verdant31 <joao.pedro3110@hotmail.com>
2024-08-07 01:10:45 -0300

    changing project to a "src folder" project

--- src/components/AnalyticsCards.tsx
+++ src/components/AnalyticsCards.tsx
@@ -1,47 +1,62 @@
-"use client";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Fii } from "@/lib/types";
-import { BRL } from "@/utils/intlBr";
+'use client'
+
+import { Fii } from '../lib/types'
+import { BRL } from '../utils/intlBr'
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
 
 interface AnalyticsCardsProps {
-  fiis: Fii[] | undefined;
-  isLoading: boolean;
+  fiis: Fii[] | undefined
+  isLoading: boolean
 }
 
 export function AnalyticsCards({ fiis }: AnalyticsCardsProps) {
-  const totalQuotes = fiis?.reduce((acc, fii) => {
-    const totalPurchaseOfFii = fii.purchases?.reduce((purchaseAcc, purchase) => {
-      return purchaseAcc + purchase.qty
-    }, 0);
-    return acc + (totalPurchaseOfFii ?? 0);
-  }, 0) ?? 0;
+  const totalQuotes =
+    fiis?.reduce((acc, fii) => {
+      const totalPurchaseOfFii = fii.purchases?.reduce(
+        (purchaseAcc, purchase) => {
+          return purchaseAcc + purchase.qty
+        },
+        0,
+      )
+      return acc + (totalPurchaseOfFii ?? 0)
+    }, 0) ?? 0
 
-  const total = fiis?.reduce((acc, fii) => {
-    const totalPurchaseOfFii = fii.purchases?.reduce((purchaseAcc, purchase) => {
-      return purchaseAcc + (purchase.qty * purchase.quotationValue)
-    }, 0);
-    return acc + (totalPurchaseOfFii ?? 0);
-  }, 0) ?? 0;
+  const total =
+    fiis?.reduce((acc, fii) => {
+      const totalPurchaseOfFii = fii.purchases?.reduce(
+        (purchaseAcc, purchase) => {
+          return purchaseAcc + purchase.qty * purchase.quotationValue
+        },
+        0,
+      )
+      return acc + (totalPurchaseOfFii ?? 0)
+    }, 0) ?? 0
 
-  const initial =  fiis?.reduce((acc, fii) => {
-    const totalPurchaseOfFii = fii.initialValue * fii.quantity
-    return acc + (totalPurchaseOfFii ?? 0);
-  }, 0) ?? 0;
+  const initial =
+    fiis?.reduce((acc, fii) => {
+      const totalPurchaseOfFii = fii.initialValue * fii.quantity
+      return acc + (totalPurchaseOfFii ?? 0)
+    }, 0) ?? 0
 
-  const percentSinceBegin = (100 * total) / initial - 100 ?? 0;
+  const percentSinceBegin = (100 * total) / initial - 100 ?? 0
 
-  const dividends = fiis?.reduce((acc, fii) => {
-    const totalPurchaseOfFii = fii.payments?.reduce((paymentAcc, payment) => {
-      return paymentAcc + (payment.quotesQuantityAtThePayment * payment.paidPerQuote);
-    }, 0);
-    return acc + (totalPurchaseOfFii ?? 0);
-  }, 0) ?? 0;
+  const dividends =
+    fiis?.reduce((acc, fii) => {
+      const totalPurchaseOfFii = fii.payments?.reduce((paymentAcc, payment) => {
+        return (
+          paymentAcc + payment.quotesQuantityAtThePayment * payment.paidPerQuote
+        )
+      }, 0)
+      return acc + (totalPurchaseOfFii ?? 0)
+    }, 0) ?? 0
 
   return (
     <aside className="flex gap-6  items-center justify-between">
       <Card className="w-[220px] bg-background border-zinc-800">
         <CardHeader>
-          <CardTitle className="text-md font-normal text-white">Total in FII's</CardTitle>
+          <CardTitle className="text-md font-normal text-white">
+            Total in FII's
+          </CardTitle>
         </CardHeader>
         <CardContent className="text-white pt-2 text-lg font-semibold tracking-wide">
           <p>{BRL.format(parseFloat(total?.toFixed(2)))}</p>
@@ -52,7 +67,9 @@ export function AnalyticsCards({ fiis }: AnalyticsCardsProps) {
       </Card>
       <Card className="w-[220px] bg-background border-zinc-800">
         <CardHeader>
-          <CardTitle className="text-md font-normal text-white">Dividends paid</CardTitle>
+          <CardTitle className="text-md font-normal text-white">
+            Dividends paid
+          </CardTitle>
         </CardHeader>
         <CardContent className="text-white pt-2 text-lg font-semibold tracking-wide">
           <p>{BRL.format(parseFloat(dividends?.toFixed(2)))}</p>
@@ -63,7 +80,9 @@ export function AnalyticsCards({ fiis }: AnalyticsCardsProps) {
       </Card>
       <Card className="w-[220px] bg-background border-zinc-800">
         <CardHeader>
-          <CardTitle className="text-md font-normal text-white">Until goal</CardTitle>
+          <CardTitle className="text-md font-normal text-white">
+            Until goal
+          </CardTitle>
         </CardHeader>
         <CardContent className="text-white pt-2 text-lg font-semibold tracking-wide">
           <p>{BRL.format(300000 - initial)}</p>
@@ -74,7 +93,9 @@ export function AnalyticsCards({ fiis }: AnalyticsCardsProps) {
       </Card>
       <Card className="w-[220px] bg-background border-zinc-800">
         <CardHeader>
-          <CardTitle className="text-md font-normal text-white">Total quotes</CardTitle>
+          <CardTitle className="text-md font-normal text-white">
+            Total quotes
+          </CardTitle>
         </CardHeader>
         <CardContent className="text-white pt-2 text-lg font-semibold tracking-wide">
           <p>{totalQuotes}</p>
@@ -84,5 +105,5 @@ export function AnalyticsCards({ fiis }: AnalyticsCardsProps) {
         </CardContent>
       </Card>
     </aside>
-  );
+  )
 }
--- src/components/MinimalFiisList.tsx
+++ src/components/MinimalFiisList.tsx
@@ -1,27 +1,43 @@
-"use client";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { getPercentFromFii } from "@/utils/getPercentFromFii";
-import { useState } from "react";
-import { BarLoader } from "react-spinners";
-import { FiiDetailsModal } from "./fiis/FiiDetailsModal";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
-import { Fii } from "@/lib/types";
+'use client'
+
+import { Table } from 'phosphor-react'
+import { useState } from 'react'
+import { BarLoader } from 'react-spinners'
+import { Fii } from '../lib/types'
+import { getPercentFromFii } from '../utils/getPercentFromFii'
+import { FiiDetailsModal } from './fiis/FiiDetailsModal'
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
+import {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './ui/table'
 
 interface MiniimalFiisListProps {
-  fiis: Fii[] | undefined;
-  isLoading: boolean;
+  fiis: Fii[] | undefined
+  isLoading: boolean
 }
 
-export default function MinimalFiisList({ fiis, isLoading }: MiniimalFiisListProps) {
-  const [detailsModalIsOpen, setDetailsModalIsOpen] = useState<Fii>();
+export default function MinimalFiisList({
+  fiis,
+  isLoading,
+}: MiniimalFiisListProps) {
+  const [detailsModalIsOpen, setDetailsModalIsOpen] = useState<Fii>()
 
   const handleOpenModal = (fii: Fii) => {
-    setDetailsModalIsOpen(fii);
-  };
+    setDetailsModalIsOpen(fii)
+  }
 
   return (
     <Card className="-mt-8 border-zinc-800 col-span-3 px-1 py-6 h-[500px] w-[900px] bg-background">
-      {!!detailsModalIsOpen && <FiiDetailsModal fii={detailsModalIsOpen} setIsOpen={setDetailsModalIsOpen} />}
+      {!!detailsModalIsOpen && (
+        <FiiDetailsModal
+          fii={detailsModalIsOpen}
+          setIsOpen={setDetailsModalIsOpen}
+        />
+      )}
       <CardHeader>
         <CardTitle className="text-white">Meus fundos imobiliários</CardTitle>
       </CardHeader>
@@ -43,30 +59,35 @@ export default function MinimalFiisList({ fiis, isLoading }: MiniimalFiisListPro
             </TableHeader>
             <TableBody>
               {fiis?.map((fii) => {
-                const fiiPercent = getPercentFromFii(fii);
+                const fiiPercent = getPercentFromFii(fii)
                 return (
-                  <TableRow className="cursor-pointer" onClick={() => handleOpenModal(fii)} key={fii.name}>
+                  <TableRow
+                    className="cursor-pointer"
+                    onClick={() => handleOpenModal(fii)}
+                    key={fii.name}
+                  >
                     <TableCell className="font-medium">{fii.name}</TableCell>
                     <TableCell>{fii.quantity}</TableCell>
                     <TableCell>
                       R${fii.quotationValue}
                       {fiiPercent !== 0 && (
-                        <span className={`${fiiPercent > 0 ? "text-green-500" : "text-red-600"} ml-2`}>
-                          {fiiPercent > 0 && "+"}
+                        <span
+                          className={`${fiiPercent > 0 ? 'text-green-500' : 'text-red-600'} ml-2`}
+                        >
+                          {fiiPercent > 0 && '+'}
                           {fiiPercent}%
                         </span>
                       )}
-                      
                     </TableCell>
                     <TableCell>{fii?.pvp}</TableCell>
                     <TableCell>{fii?.payments?.[0].date}</TableCell>
                   </TableRow>
-                );
+                )
               })}
             </TableBody>
           </Table>
         </CardContent>
       )}
     </Card>
-  );
+  )
 }
