a71e371eacb06ad28e78a9030963898ab95f0db8
verdant31 <joao.pedro3110@hotmail.com>
2024-08-16 17:42:25 -0300

    trying to fix cors errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  rewrites: () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://finance.yahoo.com/:path*',
+      },
+    ]
+  },
   experimental: {
     serverComponentsExternalPackages: ['@node-rs/argon2', 'yahoo-finance2'],
   },
