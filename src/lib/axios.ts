d0445771943a770e099a4de159664751d7ac7db9
verdant31 <joao.pedro3110@hotmail.com>
2024-08-16 17:59:11 -0300

    fixing axios base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+const apiUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+  : 'http://localhost:3000/api'
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: apiUrl,
 })
