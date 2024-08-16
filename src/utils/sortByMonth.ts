e2faf485c9518811b5bc2a5eef45ff9ff597ccb0
verdant31 <joao.pedro3110@hotmail.com>
2024-08-16 17:19:26 -0300

    wip

--- src/utils/sortByMonth.ts
+++
@@ -1,6 +0,0 @@
-export const sortByMonth = (arr: any[]) => {
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  return arr.sort(function(a, b){
-    return months.indexOf(a) - months.indexOf(b);
-  });
-}
