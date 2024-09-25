78c2793402dadaf551f330dd8ad019e4be859152
verdant31 <joao.pedro3110@hotmail.com>
2024-09-25 15:59:30 -0300

    redirect when user already logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,12 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
-import { AnimatePresence, motion } from "framer-motion";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { ClipLoader } from "react-spinners";
-import { toast } from "sonner";
-import { SignInFormData, SignInSchema } from "../types/auth";
+"../types/auth";
+import SignInForm from "@/components/sign-in-form";
+import { validateRequest } from "@/lib/validate-request";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  const router = useRouter();
+export default async function Home() {
+  const session = await validateRequest();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SignInFormData>({
-    resolver: zodResolver(SignInSchema),
-    mode: "onSubmit",
-    reValidateMode: "onSubmit",
-  });
-
-  const { mutateAsync: signInMutation, isLoading: isSigningIn } = useMutation({
-    mutationFn: async (data: SignInFormData) =>
-      await fetch("api/sign-in", {
-        method: "POST",
-        body: JSON.stringify(data),
-      }).then((res) => res.json()),
-  });
-
-  const onSubmit = async (data: SignInFormData) => {
-    const response = await signInMutation(data);
-    if (response?.status !== 200) {
-      return toast.error(response?.message);
-    }
-    router.push("/dashboard/home");
-  };
+  if (session.user?.id) redirect("/dashboard/home");
 
   return (
     <main className="fixed left-[50%] top-[50%] w-full max-w-lg translate-x-[-50%] translate-y-[-50%]">
@@ -45,67 +14,7 @@ export default function Home() {
       <p className="text-center text-muted-foreground mt-1">
         Preencha os campos abaixo para se autenticar
       </p>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y-4 max-w-[80%] mx-auto mt-6">
-          <div>
-            <div>
-              <label>Email</label>
-              <Input
-                className="mt-2"
-                placeholder="name@example.com"
-                {...register("email")}
-              />
-            </div>
-            <AnimatePresence>
-              {errors.email && (
-                <motion.p
-                  initial={{ opacity: 0, y: -10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: -10 }}
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="text-red-500 text-sm pt-2 pl-1"
-                >
-                  {errors.email.message}
-                </motion.p>
-              )}
-            </AnimatePresence>
-          </div>
-          <div>
-            <div>
-              <label>Senha</label>
-              <Input
-                placeholder="your_password@123"
-                {...register("password")}
-                type="password"
-                className="mt-2"
-              />
-            </div>
-            <AnimatePresence>
-              {errors.password && (
-                <motion.p
-                  initial={{ opacity: 0, y: -10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: -10 }}
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="text-red-500 text-sm pt-2 pl-1"
-                >
-                  {errors.password.message}
-                </motion.p>
-              )}
-            </AnimatePresence>
-          </div>
-          <Button className="w-full">
-            {isSigningIn ? <ClipLoader size={20} /> : "Entrar"}
-          </Button>
-          <p className="text-muted-foreground text-center text-sm pt-4 w-[80%] mx-auto">
-            Não tem uma conta?{" "}
-            <a href="/sign-up" className="underline">
-              Crie agora
-            </a>
-          </p>
-        </div>
-      </form>
+      <SignInForm />
     </main>
   );
 }
---
+++ src/components/sign-in-form.tsx
@@ -0,0 +1,104 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { ClipLoader } from "react-spinners";
+import { toast } from "sonner";
+import { SignInFormData, SignInSchema } from "../types/auth";
+
+export default function SignInForm() {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormData>({
+    resolver: zodResolver(SignInSchema),
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
+  });
+
+  const { mutateAsync: signInMutation, isLoading: isSigningIn } = useMutation({
+    mutationFn: async (data: SignInFormData) =>
+      await fetch("api/sign-in", {
+        method: "POST",
+        body: JSON.stringify(data),
+      }).then((res) => res.json()),
+  });
+
+  const onSubmit = async (data: SignInFormData) => {
+    const response = await signInMutation(data);
+    if (response?.status !== 200) {
+      return toast.error(response?.message);
+    }
+    router.push("/dashboard/home");
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="space-y-4 max-w-[80%] mx-auto mt-6">
+        <div>
+          <div>
+            <label>Email</label>
+            <Input
+              className="mt-2"
+              placeholder="name@example.com"
+              {...register("email")}
+            />
+          </div>
+          <AnimatePresence>
+            {errors.email && (
+              <motion.p
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="text-red-500 text-sm pt-2 pl-1"
+              >
+                {errors.email.message}
+              </motion.p>
+            )}
+          </AnimatePresence>
+        </div>
+        <div>
+          <div>
+            <label>Senha</label>
+            <Input
+              placeholder="your_password@123"
+              {...register("password")}
+              type="password"
+              className="mt-2"
+            />
+          </div>
+          <AnimatePresence>
+            {errors.password && (
+              <motion.p
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="text-red-500 text-sm pt-2 pl-1"
+              >
+                {errors.password.message}
+              </motion.p>
+            )}
+          </AnimatePresence>
+        </div>
+        <Button className="w-full">
+          {isSigningIn ? <ClipLoader size={20} /> : "Entrar"}
+        </Button>
+        <p className="text-muted-foreground text-center text-sm pt-4 w-[80%] mx-auto">
+          Não tem uma conta?{" "}
+          <a href="/sign-up" className="underline">
+            Crie agora
+          </a>
+        </p>
+      </div>
+    </form>
+  );
+}
